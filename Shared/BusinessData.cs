1ca6ca8b1648d3fb82588cfa1dc4b3241a5e0942
sandd <sandd@DESKTOP-DJUQOBR>
2020-12-07 11:09:56 +0100

    Ajoutez les fichiers .gitignore et .gitattributes.

---
+++ Shared/BusinessData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class BusinessData : IBusinessData
+    {
+        public IEnumerable<Facture> Factures => new List<Facture> {
+            new Facture("Client 1", 141, new Dates(-10, 5), new AmountDue(150f, 0f)),
+            new Facture("Client 2", 172, new Dates(-5, 15), new AmountDue(233f, 117.7f)),
+            new Facture("Client 3", 180, new Dates(-3, 9), new AmountDue(55f, 44.8f)),
+            new Facture("Client Quatre", 181, new Dates(-3, 9), new AmountDue(38f, 38f)),
+            new Facture("Client Five", 189, new Dates(-2, 18), new AmountDue(944f, 212.57f))
+        };
+
+        public float GetCurrentRevenue()
+        {
+            float sum = 0;
+            foreach (Facture facture in Factures)
+            {
+                sum += facture.AmountDue_Ticket.Paid;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Shared/Facture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturation.Shared
+{
+    public class Facture
+    {
+        public string Client { get; set; }
+        public int Number { get; set; }
+        public Dates Dates_Ticket { get; set; }
+        public AmountDue AmountDue_Ticket { get; set; }
+
+        public Facture(string client_name, int number, Dates dates, AmountDue amount)
+        {
+            Client = client_name;
+            Number = number;
+            Dates_Ticket = dates;
+            AmountDue_Ticket = amount;
+        }
+    }
+
+    public struct Dates
+    {
+        public DateTime SentOn { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Dates(int sentOn_modifier, int deadline_modifier)
+        {
+            SentOn = DateTime.Today.AddDays(sentOn_modifier); 
+            Deadline = DateTime.Today.AddDays(deadline_modifier);
+        }
+    }
+
+    public struct AmountDue
+    {
+        public float ToPay { get; set; }
+        public float Paid { get; set; }
+
+        public AmountDue(float toPay, float paid)
+        {
+            ToPay = toPay; Paid = paid;
+        }
+    }
+}
